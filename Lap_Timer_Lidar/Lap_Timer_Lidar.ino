36527041730c43f104f07be5be41dd993bff65ff
Arpan <arpan.biswas@somaiya.edu>
2020-08-24 19:09:19 +0530

    Added lap time details 1. Lap number 2. Current lap time 3. Best lap time 4. Last lap time

--- Lap_Timer_Lidar/Lap_Timer_Lidar.ino
+++ Lap_Timer_Lidar/Lap_Timer_Lidar.ino
@@ -11,8 +11,26 @@ int check; //save check value
 int i;
 int uart[9]; //save data measured by LiDAR
 const int HEADER=0x59; //frame header of data package
-unsigned long Time=0;
-unsigned long val=0;
+//unsigned long Time=0;
+//unsigned long val=0;
+
+// laps info
+unsigned long currentRunStartMillis;
+unsigned long lastRunInMillis;
+unsigned long bestRunInMillis;
+int currentLap;
+unsigned long savedMillis;
+
+// global for display
+int sec_val, milli_val;
+ 
+// laser gate
+int LidarState;             // the current reading from the lidar
+int lastLidarState = LOW;   // the previous reading from lidar
+int reading;
+unsigned long lastDebounceTime = 0;  // the last time the lidar state was toggled
+int debounceDelay = 50;    // the debounce time; increase if the output flickers
+
 void setup() {
   pinMode(8,OUTPUT);
   digitalWrite(8,HIGH);
@@ -20,7 +38,17 @@ void setup() {
   Serial1.begin(115200); //set bit rate of serial port connecting LiDAR with Arduino
   TIMSK2 = (TIMSK2 & B11111110) | 0x01;
   TCCR2B = (TCCR2B & B11111000) | 0x06;
+
+//
+  delay(50); // to let the lidar work, so we won't get the lap triggered
+  // reset params
+  currentRunStartMillis = 0;
+  lastRunInMillis = 0;
+  bestRunInMillis = 0;
+  currentLap = 0;
+//
 }
+
 
 void loop() 
 {
@@ -39,27 +67,123 @@ void loop()
  
         check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
         if (uart[8] == (check & 0xff))
-        { //verify the received data as per protocol
+        {
+          //verify the received data as per protocol
           dist = uart[2] + uart[3] * 256; //calculate distance value
           strength = uart[4] + uart[5] * 256; //calculate signal strength value
           temprature = uart[6] + uart[7] *256;//calculate chip temprature
           temprature = temprature/8 - 256;
+          
           if(dist>10)
           {
-            val=Time;
-          } 
-          Serial.println("dist = ");
+//          val=Time;
+            reading = LOW;
+          }
+          else 
+          {
+            reading = HIGH;
+          }
+//
+          if (reading != lastLidarState) 
+          {
+            // reset the debouncing timer
+            lastDebounceTime = millis();
+          } //end if
+ 
+          // if passes the debounce time
+          if ((millis() - lastDebounceTime) > debounceDelay) 
+          {
+            if (reading != LidarState) 
+            {
+              LidarState = reading;
+ 
+              // If we went high, this mean the beam was broken
+              if (LidarState == LOW) 
+              {
+              // save the millis so all the math on it will be done with the same value.
+                savedMillis = millis();
+                // if its not the first lap
+                if (currentLap > 0) 
+                {
+                  // save the last run
+                  lastRunInMillis = savedMillis - currentRunStartMillis;
+                  // if last run is faster then best run
+                  if (lastRunInMillis < bestRunInMillis || bestRunInMillis == 0) 
+                  {
+                    //save as best
+                    bestRunInMillis = lastRunInMillis;
+                  } //end if
+                } //end if
+       
+                //reset the current
+                currentRunStartMillis = savedMillis;
+       
+                // move lap counter
+                currentLap++;
+              } //end if
+            } //enf if
+          } //end if
+
+          // save the reading. Next time through the loop, it'll be the lastLidarState:
+          lastLidarState = reading;
+//          
+          Serial.print("dist = ");
           Serial.print(dist); //output measure distance value of LiDAR
-          Serial.println('\t');
-          Serial.print("time = ");
-          Serial.print(val*8); //output signal strength value Page 4
+          
+          Serial.print('\t');
+          
+//          Serial.print("strength = ");
+//          Serial.print(strength); //output signal strength value
+
+          Serial.print("Lap = ");
+//          Serial.print(val*8); //output time value
+          Serial.print(currentLap); //output current lap number
+          
+          savedMillis = millis();
+          Serial.print('\n');
+
+          if (currentLap > 0) {
+            calcResultFromMillis(savedMillis - currentRunStartMillis, &sec_val, &milli_val);
+          } 
+          else {
+            calcResultFromMillis(0, &sec_val, &milli_val);
+          } //end if
+
+          // CURRENT RUN
+          Serial.print("Current run: %02d.%03d");
+          Serial.print(sec_val, milli_val);
+
+          Serial.print('\t');
+          
+          // LAST RUN
+          calcResultFromMillis(lastRunInMillis, &sec_val, &milli_val);
+          Serial.print("Last run: %02d.%03d");
+          Serial.print(sec_val, milli_val);
+
+          Serial.print('\t');
+ 
+          // BEST RUN
+          calcResultFromMillis(bestRunInMillis, &sec_val, &milli_val);
+          Serial.print("Best run: %02d.%03d");
+          Serial.print(sec_val, milli_val);
+
+          Serial.print('\n');
         }
       }
     }
   }
 }
 
-ISR(TIMER2_OVF_vect) 
-{
-  Time = Time + 1;
+
+// calculate millis into 2 values, seconeds and millis for display
+void calcResultFromMillis(unsigned long value, int *sec_val, int *milli_val) {
+  *sec_val = int(value / 1000);
+  *milli_val = value - *sec_val * 1000;
 }
+
+
+
+//ISR(TIMER2_OVF_vect) 
+//{
+//  Time = Time + 1;
+//}
